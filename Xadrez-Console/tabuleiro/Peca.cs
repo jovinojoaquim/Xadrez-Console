1db10db837aa9ecffd19b3e3424582f19d8a8eea
Jovino Joaquim <jovinojoaquim@hotmail.com>
2019-03-26 21:12:51 -0300

    Começando a implentar o tabuleiro

---
+++ Xadrez-Console/tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro {
+    class Peca {
+
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdMovimentos { get; set; }
+        public Tabuleiro tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro) {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tabuleiro = tabuleiro;
+            this.qtdMovimentos = 0;
+        }
+    }
+}
---
+++ Xadrez-Console/tabuleiro/Posicao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro {
+    class Posicao {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna) {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+    }
+}
